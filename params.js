7873cc0015596a01625d06a7409a974db6daf69d
Dirk Simons <dhmsimons@gmail.com>
2020-07-05 20:43:57 +0200

    First Commit

---
+++ params.js
@@ -0,0 +1,23 @@
+// chacking the paramaters from the url.
+
+/**
+ * Get the URL parameters
+ * source: https://css-tricks.com/snippets/javascript/get-url-variables/
+ * @param  {String} url The URL
+ * @return {Object}     The URL parameters
+ */
+var getParams = function (url) {
+  var params = {};
+  var parser = document.createElement("a");
+  parser.href = url;
+  var query = parser.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    params[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return params;
+};
+
+let out = document.querySelector("#output");
+out.innerText = JSON.stringify(getParams(window.location.href));
